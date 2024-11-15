ade30ae131782d140705fa6eaf8a6d3d69324cb0
Riku <134082872+rigozu9@users.noreply.github.com>
2024-11-15 10:49:59 +0200

    fetches form inputs into a dictionary in app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,29 @@
 from config import app
 from flask import redirect, render_template, request, jsonify, flash
 
+from repositories.reference_repository import get_reference, create_reference
 
 @app.route("/", methods =["GET", "POST"])
 def load_index():    
+    # reference = get_reference()
     return render_template("index.html")
+
+@app.route("/create_reference", methods = ["POST"])
+def reference_creation():
+    ref_dict = {}
+    ref_dict["kirjoittajat"] = request.form.get("kirjoittajat")
+    ref_dict["otsikko"] = request.form.get("otsikko")
+    ref_dict["julkaisu"] = request.form.get("julkaisu")
+    ref_dict["vuosi"] = request.form.get("vuosi")
+    ref_dict["julkaisunumero"] = request.form.get("julkaisunumero")
+    ref_dict["sivut"] = request.form.get("sivut")
+    ref_dict["doi"] = request.form.get("DOI")
+    # return kirjoittajat, otsikko, julkaisu, DOI
+    print(ref_dict)
+    
+    # TODO
+    # reference_repository.py funktio joka postaa tietokantaan. 
+    create_reference(ref_dict)
+    # luo logiikka, tällä hetkellä create_reference funktio pass
+
+    return redirect("/")
---
+++ src/repositories/reference_repository.py
@@ -0,0 +1,6 @@
+def get_reference():
+    pass
+
+def create_reference(ref_dict: dict):
+    #tehkää 
+    pass
