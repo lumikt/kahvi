4a32c40c931d87dc938e9b463a173d70dbf86ba1
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2024-11-18 10:36:17 +0200

    taulukot done

---
+++ schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE reference (id SERIAL PRIMARY KEY,
+                         address TEXT,
+                         annote TEXT,
+                         author TEXT[],
+                         booktitle TEXT,
+                         chapter INTEGER,
+                         crossfer TEXT,
+                         doi TEXT,
+                         edition TEXT,
+                         editor TEXT,
+                         email TEXT,
+                         howpublished TEXT,
+                         institution TEXT,
+                         journal TEXT,
+                         month TEXT,
+                         note TEXT
+                         number INTEGER,
+                         organization TEXT,
+                         pages TEXT
+                         publisher TEXT,
+                         ref_type TEXT,
+                         school TEXT,
+                         series TEXT,
+                         title TEXT,
+                         type TEXT,
+                         volume INTEGER,
+                         year INTEGER);
+
+CREATE TABLE authors (id SERIAL PRIMARY KEY,
+                      name TEXT);
+
+CREATE TABLE ref_authors (ref_id INTEGER REFERENCES reference,
+                          author_id INTEGER REFERENCES authors);
+
+                         
