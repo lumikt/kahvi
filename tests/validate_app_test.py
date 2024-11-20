3b1da6375c5cb0a46410908f0e01f977fcfc2b4c
Aada Nwajei <aada.nwajei@gmail.com>
2024-11-20 19:31:59 +0200

    Unittestien alkua...

--- tests/validate_app_test.py
+++ tests/validate_app_test.py
@@ -1,1 +1,11 @@
 import unittest
+from repositories.reference_repository import create_reference, get_reference
+
+class TestArticle(unittest.TestCase):
+    def setUp(self):
+        self.dict = False
+
+    def test_mandatory_info_set(self):
+        self.dict = {"author": "Mikki Hiiri", "title": "Kerhotalo", "journal": "Disney.fi" , "year": 2012}
+        create_reference(self.dict)
+        self.assertEqual(get_reference(), ["Mikki Hiiri", "Kerhotalo", "Disney.fi", 2012])
