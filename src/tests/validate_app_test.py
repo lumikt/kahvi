69ede5061824ab7ca53441824403fe5bc93a5e1b
Sami Heinimo <sami.heinimo@helsinki.fi>
2024-11-25 11:53:08 +0200

    Started fixing pytest

--- src/tests/validate_app_test.py
+++ src/tests/validate_app_test.py
@@ -1,11 +1,13 @@
 import unittest
 from repositories.reference_repository import create_reference, get_reference
+from config import app
 
 class TestArticle(unittest.TestCase):
     def setUp(self):
         self.dict = False
 
     def test_mandatory_info_set(self):
-        self.dict = {"author": "Mikki Hiiri", "title": "Kerhotalo", "journal": "Disney.fi" , "year": 2012}
-        create_reference(self.dict)
-        self.assertEqual(get_reference(), ["Mikki Hiiri", "Kerhotalo", "Disney.fi", 2012])
+        with app.app_context():
+            self.dict = {"citation_key": "koe1001", "author": "Mikki Hiiri", "title": "Kerhotalo", "journal": "Disney.fi" , "year": 2012}
+            create_reference(self.dict, "article")
+            self.assertEqual(get_reference(), ["Mikki Hiiri", "Kerhotalo", "Disney.fi", 2012])
