889ef136a483366b1b023cc9c94c468e13ba3a56
Sami Heinimo <sami.heinimo@helsinki.fi>
2024-12-10 12:19:42 +0200

    Modified pytest so its in line with new format

--- src/tests/validate_app_test.py
+++ src/tests/validate_app_test.py
@@ -15,8 +15,7 @@ class TestArticle(unittest.TestCase):
 
     def test_mandatory_info_set(self):
         with app.app_context():
-            print(app.config["SQLALCHEMY_DATABASE_URI"])
-            self.dict = {"citation_key": "koe1001", "author": "Mikki Hiiri", "title": "Kerhotalo",
+            self.dict = {"citation_key": "koe1001", "type": "article", "author": "Mikki Hiiri", "title": "Kerhotalo",
                          "journal": "Disney.fi" , "year": 2012}
             create_reference(self.dict, "article")
-            self.assertEqual(get_reference(), ["koe1001, Mikki Hiiri, Kerhotalo, Disney.fi, 2012<br>Tags: "])
+            self.assertEqual(get_reference(), ["1, koe1001, Mikki Hiiri, Kerhotalo, Disney.fi, 2012<br>Tags: "])
